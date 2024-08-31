6ee8487cec7cdd2b4dbc5ef2d210159da8a07d7d
facu.duete <facu.duete@gmail.com>
2024-08-31 18:16:09 -0300

    Se agregó la interfaz de Login.

---
+++ PetCare/CapaPresentacion/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BIngresar_Click(object sender, EventArgs e)
+        {
+            inicio form = new inicio();
+            form.Show();
+            this.Hide();
+       
+            form.FormClosing += frm_closing;
+        }
+
+        private void frm_closing(object sender, EventArgs e)
+        {
+            this.Show();
+            TDocumento.Text = "";
+            TContraseña.Text = "";
+
+        }
+    }
+}
