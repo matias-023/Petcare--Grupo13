db2a9bb572352a322f7a7889a06e0af30e0e0dbe
Matias <maidanamatias023@gmail.com>
2024-09-04 20:17:40 -0300

    Agregando vinculaciones a la pantalla inicio

--- PetCare/CapaPresentacion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
+++ PetCare/CapaPresentacion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58938e9f303c27d7aec7d93f4f318a9d01bab3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfada1eca2a853b00287590911acffcb4987cca")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
